d544dd2cd94e1594e652718dc212d5ea34bb8e3d
sumam <sumamk08@gmail.com>
2020-09-29 21:02:07 +0530

    Updated the model

---
+++ FlightPricePrediction-Regression.py
@@ -0,0 +1,11 @@
+# Import Libraries
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the data
+data = pd.read_excel('Data_Train.xlsx')
+
+print(data.head())
